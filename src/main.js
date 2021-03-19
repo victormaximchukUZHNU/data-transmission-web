73213a682ef1b8ed0a86eaf8104e32f838949efc
victormaximchuk19 <kislovheart@gmail.com>
2021-03-19 20:52:49 +0200

    add basic ui, start implement p2p

--- src/main.js
+++ src/main.js
@@ -3,13 +3,25 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { BootstrapVue } from 'bootstrap-vue'
+import VModal from 'vue-js-modal'
+import VueSocketIO from 'vue-socket.io'
+import VueResource from 'vue-resource'
+import store from './store/index'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(VModal)
+Vue.use(VueResource)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import app from './app.store';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  app
+});
